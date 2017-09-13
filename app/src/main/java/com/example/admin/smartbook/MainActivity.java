7ea721e38027040c0e4348cd6ed1dce81c65ca30
huutrung <huutrungbkdn15@gmail.com>
2017-09-13 09:11:48 +0700

    Intent giữa 2 màn hình

--- app/src/main/java/com/example/admin/smartbook/MainActivity.java
+++ app/src/main/java/com/example/admin/smartbook/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.admin.smartbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btn_book;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btn_book = (Button) findViewById(R.id.btn_book);
+        btn_book.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentListBook = new Intent(MainActivity.this,ListBookActivity.class);
+                startActivity(intentListBook);
+
+            }
+        });
     }
 }
